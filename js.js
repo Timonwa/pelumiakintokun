cc249fe8220f571122226566951d2b01ac017d09
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-04-21 15:21:04 +0100

    added javascript menu toggle.

---
+++ js.js
@@ -0,0 +1,14 @@
+function myfunction(){
+    if (document.getElementById("open").style.display === "block"  )
+    {
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+    document.getElementById("menu").style.right = "0%";
+    }
+    else
+    {
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+    document.getElementById("menu").style.right = "-50%";
+    }
+};
