cde5f1ff6ab50d2942a63463efe4af57f64530ab
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-04-21 16:32:51 +0100

    added the link to the third project and made the menu on smaller screen close when you click on the nav links.

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-function myfunction(){
+function menu(){
     if (document.getElementById("open").style.display === "block"  )
     {
     document.getElementById("open").style.display = "none";
@@ -12,3 +12,9 @@ function myfunction(){
     document.getElementById("menu").style.right = "-50%";
     }
 };
+
+function navLinkCloseMenu(){
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+    document.getElementById("menu").style.right = "-50%"; 
+}
